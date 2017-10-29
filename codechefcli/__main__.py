2d501b2900b394ef6c4705d8157bc87a7ce57ff5
Sachin Kukreja <sachin.kukreja@iiitdmj.ac.in>
2017-10-30 04:01:01 +0530

    Added functionality to get problem description

--- codechefcli/__main__.py
+++ codechefcli/__main__.py
@@ -3,6 +3,7 @@ import sys
 from getpass import getpass
 
 from .auth import login, logout, get_other_active_sessions
+from .problems import get_description
 
 
 try:
@@ -26,6 +27,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--login', '-l', required=False, nargs='?', metavar='username', default='##no_login##')
     parser.add_argument('--logout', required=False, action='store_true')
+    parser.add_argument('--problem', '-p', required=False)
     if len(sys.argv) == 1:
         parser.print_help()
         exit(0)
@@ -38,6 +40,7 @@ def main():
 
     username = args['login']
     is_logout = args['logout']
+    problem_code = args['problem']
 
     if username != '##no_login##':
         prompt('login', username=username)
@@ -47,6 +50,10 @@ def main():
         logout()
         exit(0)
 
+    if problem_code:
+        desc = get_description(problem_code)
+        if desc:
+            print (desc)
 
 if __name__ == '__main__':
     main()
