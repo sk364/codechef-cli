607f59b7bc0708a3fc7386788a0c5f8d5a126baa
Sachin Kukreja <sachin.kukreja@walmartlabs.com>
2020-06-13 02:30:55 +0530

    Bump version to 0.4.3

--- setup.py
+++ setup.py
@@ -11,24 +11,22 @@ if sys.version < '2.0.0':
     sys.exit(1)
 
 with open('README.rst') as f:
-    longd = f.read()
+    longd = f'\n\n{f.read()}'
 
 setup(
     name='codechefcli',
     include_package_data=True,
-    packages=["codechefcli", "codechefcli.utils"],
-    data_files=[('codechefcli', []), ('codechefcli.utils', [])],
+    packages=["codechefcli"],
+    data_files=[('codechefcli', [])],
     entry_points={"console_scripts": ['codechefcli = codechefcli.__main__:main']},
-    install_requires=['BeautifulSoup4', 'requests'],
+    install_requires=['requests_html'],
     python_requires='>=2.7',
-    requires=['BeautifulSoup4', 'requests'],
-    version='0.4.2',
+    version='0.4.3',
     url='http://www.github.com/sk364/codechef-cli',
-    keywords="codechefcli codechef cli programming",
+    keywords="codechefcli codechef cli programming competitive-programming competitive-coding",
     license='GNU',
     author='Sachin Kukreja',
     author_email='skad5455@gmail.com',
-    description='CodeChef command line interface. CodeChefCLI helps competitive coders to search, view,\
-                 and submit problems in CodeChef.',
-    long_description="\n\n"+longd
+    description='CodeChef Command Line Interface',
+    long_description=longd
 )
