d84826b41f2c50c2b985cfbde5092d55ab9fc051
Sachin Kukreja <sachin.kukreja@walmartlabs.com>
2020-06-12 16:54:07 +0530

    Bump version to 0.4.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     install_requires=['BeautifulSoup4', 'requests'],
     python_requires='>=2.7',
     requires=['BeautifulSoup4', 'requests'],
-    version='0.4',
+    version='0.4.1',
     url='http://www.github.com/sk364/codechef-cli',
     keywords="codechefcli codechef cli programming",
     license='GNU',
