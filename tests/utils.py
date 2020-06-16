bca34ee05b5c7b9c2bffe0342cb88c43374f07ae
Sachin Kukreja <sachin.kukreja@walmartlabs.com>
2020-06-16 08:40:11 +0530

    Added kwarg url in mock response class

--- tests/utils.py
+++ tests/utils.py
@@ -1,7 +1,10 @@
 from requests_html import HTML
 
+from codechefcli.helpers import BASE_URL
+
 
 class MockHTMLResponse:
-    def __init__(self, data='<html />', status_code=200):
+    def __init__(self, data='<html />', status_code=200, url=''):
         self.html = HTML(html=data)
         self.status_code = status_code
+        self.url = f'{BASE_URL}{url}'
