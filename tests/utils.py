e49e9f79e7bedf03dc83d052417bc89666265644
Sachin Kukreja <sachin.kukreja@walmartlabs.com>
2020-06-17 13:53:46 +0530

    Added json support in mock response class

--- tests/utils.py
+++ tests/utils.py
@@ -1,10 +1,16 @@
+import json
+
 from requests_html import HTML
 
 from codechefcli.helpers import BASE_URL
 
 
 class MockHTMLResponse:
-    def __init__(self, data='<html />', status_code=200, url=''):
+    def __init__(self, data='<html />', status_code=200, url='', json=""):
         self.html = HTML(html=data)
         self.status_code = status_code
         self.url = f'{BASE_URL}{url}'
+        self.text = json
+
+    def json(self, **kwargs):
+        return json.loads(self.text)
