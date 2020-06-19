04d1e6ed62c99bd20f7a674f4fd8a80396ad7bc8
Sachin Kukreja <sachin.kukreja@walmartlabs.com>
2020-06-19 19:18:13 +0530

    Added fake login method

--- tests/utils.py
+++ tests/utils.py
@@ -2,7 +2,7 @@ import json
 
 from requests_html import HTML
 
-from codechefcli.helpers import BASE_URL
+from codechefcli.helpers import BASE_URL, COOKIES_FILE_PATH
 
 
 class MockHTMLResponse:
@@ -14,3 +14,10 @@ class MockHTMLResponse:
 
     def json(self, **kwargs):
         return json.loads(self.text)
+
+
+def fake_login():
+    """Fake login by creating cookies file having a fake cookie"""
+    with open(COOKIES_FILE_PATH, 'w') as f:
+        f.write('#LWP-Cookies-1.0\nSet-Cookie3: mykey=myvalue; path="/"; domain=localhost; port=80000; \
+            expires="2120-05-05 23:40:21Z"; version=0')
