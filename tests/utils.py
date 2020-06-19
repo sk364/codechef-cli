1d567336d77b08f8160ec5c82504691e18bd0df9
Sachin Kukreja <sachin.kukreja@walmartlabs.com>
2020-06-19 19:36:46 +0530

    Added init_cookies kwarg in fake login method to add custom cookies

--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from requests_html import HTML
 
@@ -16,8 +17,20 @@ class MockHTMLResponse:
         return json.loads(self.text)
 
 
-def fake_login():
+def fake_login(init_cookies=[]):
     """Fake login by creating cookies file having a fake cookie"""
+    cookies = ''
+    if init_cookies:
+        cookies = "\n".join([
+            f"Set-Cookie3: {cookie['name']}={cookie['value']}; path='/'; domain=localhost; \
+            port=80000; expires='2120-05-05 23:40:21Z'; version=0" for cookie in init_cookies
+        ])
     with open(COOKIES_FILE_PATH, 'w') as f:
-        f.write('#LWP-Cookies-1.0\nSet-Cookie3: mykey=myvalue; path="/"; domain=localhost; port=80000; \
-            expires="2120-05-05 23:40:21Z"; version=0')
+        f.write(f'#LWP-Cookies-1.0\nSet-Cookie3: mykey=myvalue; path="/"; domain=localhost; port=80000; \
+            expires="2120-05-05 23:40:21Z"; version=0\n{cookies}')
+
+
+def fake_logout():
+    """Fake logout by deleting the cookies"""
+    if os.path.exists(COOKIES_FILE_PATH):
+        os.remove(COOKIES_FILE_PATH)
