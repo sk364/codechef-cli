90a075d9b67958b270c9b0cb9d29e702a3dc6a2d
Ashish Shevale <shevaleashish@gmail.com>
2020-11-23 14:26:19 +0530

    Added docstrings to methods in helpers module of package

--- codechefcli/helpers.py
+++ codechefcli/helpers.py
@@ -28,10 +28,20 @@ BCOLORS = {
 
 
 def set_session_cookies(session):
+    """Adds any existing cookies to the current session
+
+    Args:
+      session: HTMLSession object representing users current session
+    """
     session.cookies = LWPCookieJar(filename=COOKIES_FILE_PATH)
 
 
 def get_session():
+    """Returns a HTMLSession object representing the users current session
+
+    If the user has not logged in, returns a new HTMLSession object
+    Otherwise, copies the cookies to the session object and returns it
+    """
     session = HTMLSession()
 
     if os.path.exists(COOKIES_FILE_PATH):
@@ -41,6 +51,8 @@ def get_session():
 
 
 def init_session_cookie(name, value, **kwargs):
+    """Creates and returns a new Cookie for current session
+    """
     return Cookie(version=0, name=name, value=value, port=None, port_specified=False,
                   domain='www.codechef.com', domain_specified=False, domain_initial_dot=False,
                   path='/', path_specified=True, secure=False, expires=None, discard=False,
@@ -48,6 +60,10 @@ def init_session_cookie(name, value, **kwargs):
 
 
 def get_username():
+    """Returns the username of current user
+
+    Analyzes cookies from current session and gets the username
+    """
     session = get_session()
 
     for index, cookie in enumerate(session.cookies):
@@ -58,6 +74,11 @@ def get_username():
 
 
 def request(session=None, method="GET", url="", token=None, **kwargs):
+    """Makes a HTTP request from current session
+
+    Returns:
+      result after making the HTTP request
+    """
     if not session:
         session = get_session()
     if token:
@@ -75,6 +96,8 @@ def request(session=None, method="GET", url="", token=None, **kwargs):
 
 
 def html_to_list(table):
+    """Retrieves table from HTML and converts it to nested list
+    """
     if not table:
         return []
 
@@ -86,6 +109,9 @@ def html_to_list(table):
 
 
 def get_col_max_lengths(data_rows, num_cols):
+    """Calculates the maximum number of characters in every row of data
+    Used when printing a table with large text in it
+    """
     max_len_in_cols = [0] * num_cols
     for row in data_rows:
         for index, val in enumerate(row):
@@ -95,6 +121,22 @@ def get_col_max_lengths(data_rows, num_cols):
 
 
 def print_table(data_rows, min_num_spaces=MIN_NUM_SPACES, is_pager=True):
+    """Prints the table passed to it.
+
+    Used for printing the list of problems from CodeChef
+
+    Args:
+      data_rows:
+        table containing the data to be printed
+      min_num_spaces:
+        minimum number of spaces to print between two columns
+      is_pager:
+        bool value indicating whether to print the table using pager module in pydoc
+
+    Returns:
+      String to be printed. The string is obtained from the table, with appropriate
+      number of spaces added to make it look more uniform.
+    """
     if len(data_rows) == 0:
         return
 
@@ -116,6 +158,18 @@ def print_table(data_rows, min_num_spaces=MIN_NUM_SPACES, is_pager=True):
 
 
 def style_text(text, color=None):
+    """Styles text in different colors and returns it
+
+    Args:
+      text:
+        text to be styled
+      color:
+        color in which text is to be printed
+
+    Returns:
+      text styled in the appropriate format, or in case of invalid color the
+      original text
+    """
     if color is None or BCOLORS.get(color) is None:
         return text
 
@@ -123,6 +177,24 @@ def style_text(text, color=None):
 
 
 def print_response_util(data, extra, data_type, color, is_pager=True):
+    """Prints the given data in an appropriate format
+
+    Changes the given string in the given color and returns the new string
+
+    Args:
+      data:
+        the string to be printed
+      extra:
+        additional text to be styled
+      data_type:
+        type of data to be printed, "text", or "table"
+      is_pager:
+        bool value indicating whether to use the pager method in pydoc package
+
+    Returns:
+      The data string formatted in the appropriate color
+      The extra string formatted in the appropriate color
+    """
     if data is None and extra is None:
         no_data_msg = style_text('Nothing to show.', 'WARNING')
         print(no_data_msg)
@@ -146,6 +218,25 @@ def print_response_util(data, extra, data_type, color, is_pager=True):
 
 
 def print_response(data_type='text', code=200, data=None, extra=None, **kwargs):
+    """Prints the given text in appropriate format.
+
+    Based on whether any data was passed and the HTTP repsonse code given, this
+    method prints the data (or an error message) in an appropriate color
+
+    Args:
+      data_type:
+        type of data to be printed, "text" ot "table"
+      code:
+        the http response code, used for determining the color of text.
+        (ex. errors are printed in red, etc.)
+      data:
+        the data to be printed
+      is_pager:
+        optional parameter whether to print data using pager method in pydoc package
+
+    Returns:
+      A string formatted in the appropriate format
+    """
     color = None
 
     if code == 503:
@@ -169,5 +260,7 @@ def print_response(data_type='text', code=200, data=None, extra=None, **kwargs):
 
 
 def get_csrf_token(rhtml, selector):
+    """
+    """
     token = rhtml.find(f"#{selector}", first=True)
     return token and hasattr(token.element, 'value') and token.element.value
