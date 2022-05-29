8905224036b0771b10dccdad6c37976d5e2bc116
Sachin <skad5455@gmail.com>
2022-05-29 20:49:04 +0530

    Fix login url, request data and response handling

--- codechefcli/auth.py
+++ codechefcli/auth.py
@@ -9,13 +9,14 @@ from codechefcli.helpers import (COOKIES_FILE_PATH, CSRF_TOKEN_INPUT_ID, get_csr
 
 CSRF_TOKEN_MISSING = 'No CSRF Token found'
 SESSION_LIMIT_FORM_ID = '#session-limit-page'
-LOGIN_FORM_ID = '#new_login_form'
+LOGIN_FORM_ID = 'ajax_login_form'
 LOGOUT_BUTTON_CLASS = '.logout-link'
 EMPTY_AUTH_DATA_MSG = 'Username/Password field cannot be left blank.'
 SESSION_LIMIT_MSG = 'Session limit exceeded!'
 INCORRECT_CREDS_MSG = 'Incorrect Credentials!'
 LOGIN_SUCCESS_MSG = 'Successfully logged in!'
 LOGOUT_SUCCESS_MSG = 'Successfully logged out!'
+LOGIN_URL = "https://www.codechef.com/api/codechef/login"
 
 
 def is_logged_in(resp):
@@ -48,7 +49,6 @@ def disconnect_active_sessions(session, login_resp_html):
 
 def save_session_cookies(session, username):
     session.cookies.set_cookie(init_session_cookie("username", username))
-    session.cookies.clear('www.codechef.com', '/', 'login_logout')
     session.cookies.save(ignore_expires=True, ignore_discard=True)
 
 
@@ -56,31 +56,17 @@ def make_login_req(username, password, disconnect_sessions):
     with HTMLSession() as session:
         set_session_cookies(session)
 
-        resp = request(session=session)
-        token = get_csrf_token(resp.html, CSRF_TOKEN_INPUT_ID)
-        if not token:
-            return [{'data': CSRF_TOKEN_MISSING, 'code': 500}]
-
         data = {
             'name': username,
             'pass': password,
-            'form_id': LOGIN_FORM_ID[1:],
-            'csrfToken': token
+            'form_id': LOGIN_FORM_ID,
         }
 
-        resp = request(session=session, method='POST', data=data)
-        resp_html = resp.html
+        resp = request(url=LOGIN_URL, session=session, method='POST', data=data)
+        resp_json = resp.json()
 
         if resp.status_code == 200:
-            if resp_html.find(SESSION_LIMIT_FORM_ID):
-                if disconnect_sessions:
-                    resps = disconnect_active_sessions(session, resp_html)
-                    save_session_cookies(session, username)
-                    return resps
-                else:
-                    logout(session=session)
-                    return [{'data': SESSION_LIMIT_MSG, 'code': 400}]
-            elif resp_html.find(LOGOUT_BUTTON_CLASS):
+            if resp_json.get('status') == "success":
                 save_session_cookies(session, username)
                 return [{'data': LOGIN_SUCCESS_MSG}]
             return [{'data': INCORRECT_CREDS_MSG, 'code': 400}]
